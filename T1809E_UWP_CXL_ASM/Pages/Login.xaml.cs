62e96c657ed407b87b28e534526513cc7d1674c8
LinhRua <44184455+LinhRua@users.noreply.github.com>
2020-01-10 18:59:08 +0700

    LinhHiHi
    Linh

---
+++ T1809E_UWP_CXL_ASM/Pages/Login.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using T1809E_UWP_CXL_ASM.Utils;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace T1809E_UWP_CXL_ASM.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+
+    public sealed partial class Login : Page
+    {
+        private readonly Validator validator = new Validator();
+
+        public Login()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void LoginHanlde(object sender, RoutedEventArgs e)
+        {
+            if (validator.IsEmail(email.Text))
+            {
+                emailAlert.Text = "";
+                if (validator.IsNotNullAndNotEmpty(password.Password)){
+                    passwordAlert.Text = "";
+                    //Next
+                }
+                else
+                {
+                    passwordAlert.Text = "Please enter password";
+                }
+            }
+            else
+            {
+                var content = new LoginNTT
+                {
+                    email = email.Text,
+                    password = password.Password
+                };
+                var httpClient = new HttpClient();
+                var json = JsonConvert.SerializeObject(content);
+                var para = new StringContent(json, Encoding.UTF8, "application/json");
+                var response =
+                    await httpClient.PostAsync(
+                        "https://2-dot-backup-server-002.appspot.com/_api/v2/members/authentication",
+                        para);
+                var resp = await response.Content.ReadAsStringAsync();
+                LoginTk member = JsonConvert.DeserializeObject<LoginTk>(resp);
+
+                var Url = Path.Combine(
+                    "C://Users//LinhRua//source//repos//T1809E_UWP_CXL_ASM//T1809E_UWP_CXL_ASM//Assets//token.txt");
+                System.IO.File.WriteAllText(Url, member.token.ToString());
+            }
+
+            {
+            emailAlert.Text = "Inlavid email, please enter valid email format.";
+        }
+    }
+        public class LoginNTT
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+        public class LoginTk
+        {
+            public string token { get; set; }
+            public string secretToken { get; set; }
+
+        }
+    }
+}
---
+++ T1809E_UWP_CXL_ASM/Services/SongService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T1809E_UWP_CXL_ASM.Models;
+
+namespace T1809E_UWP_CXL_ASM.Services
+{
+    class SongService
+    {
+        public ObservableCollection<Song> LoadSong()
+        {
+            ObservableCollection<Song> list = new ObservableCollection<Song>();
+            list.Add(new Song()
+            {
+                SongName = "Mượn rượu tỏ tình",
+                Link = " https://c1-ex-swe.nixcdn.com/NhacCuaTui976/MuonRuouToTinh-EmilyBigDaddy-5871420.mp3",
+                Views = 123,
+                Description = "Mượn rượu tỏ tình",
+                Thumbnail =
+                    "https://znews-photo.zadn.vn/w1024/Uploaded/qfssu/2019_02_08/batch_dsc_2961_1.jpg"
+            });
+            list.Add(new Song()
+            {
+                SongName = "Lang Thang",
+                Link = "https://c1-ex-swe.nixcdn.com/NhacCuaTui993/LangThang-Rhymastic-6184222.mp3",
+                Views = 456,
+                Description = "Lang Thang",
+                Thumbnail = "https://photo-zmp3.zadn.vn/cover_artist/e/0/e09915ec25d2d8e52aaf0d9381121da8_1503980596.jpg"
+            });
+            list.Add(new Song()
+            {
+                SongName = "7 Tỷ Người",
+                Link = "https://vnno-vn-5-tf-mp3-s1-zmp3.zadn.vn/db0ccc32ee75072b5e64/633399884026569431?authen=exp=1578824447~acl=/db0ccc32ee75072b5e64/*~hmac=5d064c5020e1f869e81febe8f4393fb5&filename=7-Ty-Nguoi-Piano-Version-Acy-Xuan-Tai-Lynk-Lee.mp3",
+                Views = 789,
+                Description = "Piano Vision ",
+                Thumbnail = "https://lh4.googleusercontent.com/3MbANHBCgyD88W9FtHF32gXsutBQZw4mIpzRFhw54uDJfpniNMmoIe9MIpGXpP-HAkZgiAS2hF5P2HDFMBpZIIioqXZVBDOmfU_t6nzFLGnAID1tYIS_uVejUtYwqA"
+            });
+
+            return list;
+        }
+    }
+}
